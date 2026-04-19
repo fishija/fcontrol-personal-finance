1e6b2379f3cd8f1fb347f4953d14139fbcdab4b8
Jakub Mikuła <jakub.mikula64@gmail.com>
2026-04-19 13:53:15 +0200

    Update pockets_widget with basic ui and add, delete functionalities
    for now edit is just a placeholder - TBD

--- src/fcontrol/ui/qt_generated/pockets_widget.py
+++ src/fcontrol/ui/qt_generated/pockets_widget.py
@@ -15,16 +15,49 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QLabel, QSizePolicy, QWidget)
+from PySide6.QtWidgets import (QAbstractSpinBox, QApplication, QComboBox, QDoubleSpinBox,
+    QHeaderView, QLabel, QLineEdit, QPushButton,
+    QSizePolicy, QTableWidget, QTableWidgetItem, QWidget)
 
 class Ui_PocketsWidget(object):
     def setupUi(self, PocketsWidget):
         if not PocketsWidget.objectName():
             PocketsWidget.setObjectName(u"PocketsWidget")
-        PocketsWidget.resize(499, 444)
+        PocketsWidget.resize(488, 483)
         self.label = QLabel(PocketsWidget)
         self.label.setObjectName(u"label")
-        self.label.setGeometry(QRect(240, 170, 60, 16))
+        self.label.setGeometry(QRect(210, 10, 60, 16))
+        self.label_2 = QLabel(PocketsWidget)
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setGeometry(QRect(110, 70, 58, 16))
+        self.label_3 = QLabel(PocketsWidget)
+        self.label_3.setObjectName(u"label_3")
+        self.label_3.setGeometry(QRect(110, 100, 58, 16))
+        self.label_4 = QLabel(PocketsWidget)
+        self.label_4.setObjectName(u"label_4")
+        self.label_4.setGeometry(QRect(110, 130, 61, 16))
+        self.currencySelect = QComboBox(PocketsWidget)
+        self.currencySelect.setObjectName(u"currencySelect")
+        self.currencySelect.setGeometry(QRect(180, 120, 81, 32))
+        self.nameInput = QLineEdit(PocketsWidget)
+        self.nameInput.setObjectName(u"nameInput")
+        self.nameInput.setGeometry(QRect(192, 70, 111, 21))
+        self.balanceInput = QDoubleSpinBox(PocketsWidget)
+        self.balanceInput.setObjectName(u"balanceInput")
+        self.balanceInput.setGeometry(QRect(190, 100, 91, 22))
+        self.balanceInput.setButtonSymbols(QAbstractSpinBox.ButtonSymbols.NoButtons)
+        self.addButton = QPushButton(PocketsWidget)
+        self.addButton.setObjectName(u"addButton")
+        self.addButton.setGeometry(QRect(320, 140, 100, 32))
+        self.deleteButton = QPushButton(PocketsWidget)
+        self.deleteButton.setObjectName(u"deleteButton")
+        self.deleteButton.setGeometry(QRect(359, 420, 81, 32))
+        self.pocketsTable = QTableWidget(PocketsWidget)
+        self.pocketsTable.setObjectName(u"pocketsTable")
+        self.pocketsTable.setGeometry(QRect(40, 190, 401, 231))
+        self.editButton = QPushButton(PocketsWidget)
+        self.editButton.setObjectName(u"editButton")
+        self.editButton.setGeometry(QRect(270, 420, 81, 32))
 
         self.retranslateUi(PocketsWidget)
 
@@ -34,5 +67,11 @@ class Ui_PocketsWidget(object):
     def retranslateUi(self, PocketsWidget):
         PocketsWidget.setWindowTitle(QCoreApplication.translate("PocketsWidget", u"Form", None))
         self.label.setText(QCoreApplication.translate("PocketsWidget", u"Pockets", None))
+        self.label_2.setText(QCoreApplication.translate("PocketsWidget", u"Name", None))
+        self.label_3.setText(QCoreApplication.translate("PocketsWidget", u"Balance", None))
+        self.label_4.setText(QCoreApplication.translate("PocketsWidget", u"Currency", None))
+        self.addButton.setText(QCoreApplication.translate("PocketsWidget", u"Add Pocket", None))
+        self.deleteButton.setText(QCoreApplication.translate("PocketsWidget", u"Delete", None))
+        self.editButton.setText(QCoreApplication.translate("PocketsWidget", u"Edit", None))
     # retranslateUi
 
--- src/fcontrol/ui/views/pockets_widget.py
+++ src/fcontrol/ui/views/pockets_widget.py
@@ -1,9 +1,123 @@
-from PySide6.QtWidgets import QWidget
+from PySide6.QtWidgets import QWidget, QTableWidget, QTableWidgetItem, QMessageBox
+from PySide6.QtCore import Qt, Signal
 
 from fcontrol.ui.qt_generated.pockets_widget import Ui_PocketsWidget
+from fcontrol.models.pocket import Pocket
+from fcontrol.config import CURRENCIES
 
 
 class PocketsWidget(Ui_PocketsWidget, QWidget):
+    add_request = Signal(str, float, str)  # pocket name, balance, currency
+    delete_request = Signal(int)  # pocket id
+
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+
+        self._setup_inputs()
+        self._setup_table()
+        self._connect_signals()
+
+        # Disable delete and edit buttons until any pocket is selected
+        self.deleteButton.setEnabled(False)
+        self.editButton.setEnabled(False)
+
+    def _setup_inputs(self):
+        self.nameInput.setPlaceholderText("Pocket Name")
+
+        self.balanceInput.setMinimum(0)
+        self.balanceInput.setMaximum(1_000_000_000)
+        self.balanceInput.setDecimals(2)
+        self.balanceInput.setValue(0.00)
+
+        self.currencySelect.addItems(CURRENCIES)
+
+    def _setup_table(self):
+        self.pocketsTable.setColumnCount(3)
+        self.pocketsTable.setHorizontalHeaderLabels(["Name", "Balance", "Currency"])
+
+        self.pocketsTable.setSelectionMode(QTableWidget.SelectionMode.SingleSelection)
+        self.pocketsTable.setSelectionBehavior(
+            QTableWidget.SelectionBehavior.SelectRows
+        )
+        self.pocketsTable.setEditTriggers(QTableWidget.EditTrigger.NoEditTriggers)
+
+    def _connect_signals(self):
+        self.pocketsTable.itemSelectionChanged.connect(self._on_table_selection_changed)
+        self.pocketsTable.itemDoubleClicked.connect(self._on_double_clicked)
+
+        self.addButton.clicked.connect(self._on_add_clicked)
+        self.editButton.clicked.connect(self._on_edit_clicked)
+        self.deleteButton.clicked.connect(self._on_delete_clicked)
+
+    def _on_table_selection_changed(self):
+        selected_items = self.pocketsTable.selectedItems()
+        self.deleteButton.setEnabled(bool(selected_items))
+        self.editButton.setEnabled(bool(selected_items))
+
+    def _get_selected_pocket_id(self) -> int | None:
+        selected_items = self.pocketsTable.selectedItems()
+        if not selected_items:
+            return None
+        return selected_items[0].data(Qt.ItemDataRole.UserRole)
+
+    def _ask_for_confirmation(self, message: str) -> bool:
+        reply = QMessageBox.question(
+            self,
+            "Confirm Action",
+            message,
+            QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No,
+        )
+        return reply == QMessageBox.StandardButton.Yes
+
+    def _set_style_invalid(self, widget, is_invalid: bool):
+        if is_invalid:
+            widget.setStyleSheet("border: 1px solid red;")
+        else:
+            widget.setStyleSheet("")
+
+    def _on_add_clicked(self):
+        name = self.nameInput.text().strip()
+        balance = self.balanceInput.value()
+        currency = self.currencySelect.currentText()
+
+        # perform input validation
+        if not name:
+            self._set_style_invalid(self.nameInput, True)
+            return
+        else:
+            self._set_style_invalid(self.nameInput, False)
+
+        self.add_request.emit(name, balance, currency)
+
+    def _on_delete_clicked(self):
+        pocket_id = self._get_selected_pocket_id()
+        if pocket_id is not None:
+            confirmation = self._ask_for_confirmation(
+                "Are you sure you want to delete the selected pocket?"
+            )
+            if not confirmation:
+                return
+
+            self.delete_request.emit(pocket_id)
+
+    def _on_edit_clicked(self):
+        # TODO: emit edit request with selected pocket ID
+        pass
+
+    def _on_double_clicked(self, item):
+        # TODO: emit edit request with selected pocket ID
+        pass
+
+    def populate_table(self, pockets: list[Pocket]):
+        self.pocketsTable.setRowCount(0)
+        for pocket in pockets:
+            row = self.pocketsTable.rowCount()
+            self.pocketsTable.insertRow(row)
+
+            name_item = QTableWidgetItem(pocket.name)
+            name_item.setData(Qt.ItemDataRole.UserRole, pocket.id)
+
+            self.pocketsTable.setItem(row, 0, name_item)
+            self.pocketsTable.setItem(row, 1, QTableWidgetItem(f"{pocket.balance:.2f}"))
+            self.pocketsTable.setItem(row, 2, QTableWidgetItem(pocket.currency))
