05ab7b85a6a24cb889e8bf2f8d1838ae5f24b2d9
Jakub Mikuła <jakub.mikula64@gmail.com>
2026-04-27 22:29:07 +0200

    Add category management to TransactionsWidget (UI only)

--- src/fcontrol/ui/qt_generated/transactions_widget.py
+++ src/fcontrol/ui/qt_generated/transactions_widget.py
@@ -15,17 +15,45 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QListWidget, QListWidgetItem, QSizePolicy,
+from PySide6.QtWidgets import (QApplication, QGroupBox, QLabel, QLineEdit,
+    QListWidget, QListWidgetItem, QPushButton, QSizePolicy,
     QWidget)
 
 class Ui_TransactionsWidget(object):
     def setupUi(self, TransactionsWidget):
         if not TransactionsWidget.objectName():
             TransactionsWidget.setObjectName(u"TransactionsWidget")
-        TransactionsWidget.resize(400, 300)
+        TransactionsWidget.resize(626, 455)
         self.transactionsList = QListWidget(TransactionsWidget)
         self.transactionsList.setObjectName(u"transactionsList")
-        self.transactionsList.setGeometry(QRect(20, 10, 361, 281))
+        self.transactionsList.setGeometry(QRect(250, 50, 361, 391))
+        self.groupBox = QGroupBox(TransactionsWidget)
+        self.groupBox.setObjectName(u"groupBox")
+        self.groupBox.setGeometry(QRect(20, 50, 221, 71))
+        self.nameInput = QLineEdit(self.groupBox)
+        self.nameInput.setObjectName(u"nameInput")
+        self.nameInput.setGeometry(QRect(10, 30, 131, 21))
+        self.label_3 = QLabel(self.groupBox)
+        self.label_3.setObjectName(u"label_3")
+        self.label_3.setGeometry(QRect(10, 10, 58, 16))
+        self.addButton = QPushButton(self.groupBox)
+        self.addButton.setObjectName(u"addButton")
+        self.addButton.setGeometry(QRect(150, 20, 61, 32))
+        self.infoLabel = QLabel(self.groupBox)
+        self.infoLabel.setObjectName(u"infoLabel")
+        self.infoLabel.setGeometry(QRect(10, 50, 191, 16))
+        self.label = QLabel(TransactionsWidget)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(350, 20, 131, 16))
+        self.label_2 = QLabel(TransactionsWidget)
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setGeometry(QRect(90, 20, 91, 16))
+        self.categoriesList = QListWidget(TransactionsWidget)
+        self.categoriesList.setObjectName(u"categoriesList")
+        self.categoriesList.setGeometry(QRect(20, 130, 221, 281))
+        self.deleteButton = QPushButton(TransactionsWidget)
+        self.deleteButton.setObjectName(u"deleteButton")
+        self.deleteButton.setGeometry(QRect(160, 410, 81, 32))
 
         self.retranslateUi(TransactionsWidget)
 
@@ -34,5 +62,12 @@ class Ui_TransactionsWidget(object):
 
     def retranslateUi(self, TransactionsWidget):
         TransactionsWidget.setWindowTitle(QCoreApplication.translate("TransactionsWidget", u"Form", None))
+        self.groupBox.setTitle("")
+        self.label_3.setText(QCoreApplication.translate("TransactionsWidget", u"Name", None))
+        self.addButton.setText(QCoreApplication.translate("TransactionsWidget", u"Add", None))
+        self.infoLabel.setText(QCoreApplication.translate("TransactionsWidget", u"TextLabel", None))
+        self.label.setText(QCoreApplication.translate("TransactionsWidget", u"Transaciton list", None))
+        self.label_2.setText(QCoreApplication.translate("TransactionsWidget", u"Categories", None))
+        self.deleteButton.setText(QCoreApplication.translate("TransactionsWidget", u"Delete", None))
     # retranslateUi
 
