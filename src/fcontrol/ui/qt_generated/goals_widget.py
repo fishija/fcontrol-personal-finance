15d89d5ac1d746033d473fe217585c6e2818eb93
Jakub Mikuła <jakub.mikula64@gmail.com>
2026-05-05 23:39:16 +0200

    Add "Set target date" checkbox to goals widget to meet target_date NULL allowed in db

--- src/fcontrol/ui/qt_generated/goals_widget.py
+++ src/fcontrol/ui/qt_generated/goals_widget.py
@@ -15,10 +15,10 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QAbstractSpinBox, QApplication, QComboBox, QDateEdit,
-    QDoubleSpinBox, QGroupBox, QLabel, QLineEdit,
-    QListWidget, QListWidgetItem, QPushButton, QSizePolicy,
-    QTextEdit, QWidget)
+from PySide6.QtWidgets import (QAbstractSpinBox, QApplication, QCheckBox, QComboBox,
+    QDateEdit, QDoubleSpinBox, QGroupBox, QLabel,
+    QLineEdit, QListWidget, QListWidgetItem, QPushButton,
+    QSizePolicy, QTextEdit, QWidget)
 
 class Ui_GoalsWidget(object):
     def setupUi(self, GoalsWidget):
@@ -27,7 +27,7 @@ class Ui_GoalsWidget(object):
         GoalsWidget.resize(474, 360)
         self.groupBox = QGroupBox(GoalsWidget)
         self.groupBox.setObjectName(u"groupBox")
-        self.groupBox.setGeometry(QRect(20, 10, 421, 131))
+        self.groupBox.setGeometry(QRect(30, 10, 421, 151))
         self.addButton = QPushButton(self.groupBox)
         self.addButton.setObjectName(u"addButton")
         self.addButton.setGeometry(QRect(310, 100, 100, 32))
@@ -43,9 +43,9 @@ class Ui_GoalsWidget(object):
         self.label_3 = QLabel(self.groupBox)
         self.label_3.setObjectName(u"label_3")
         self.label_3.setGeometry(QRect(10, 40, 58, 16))
-        self.label_4 = QLabel(self.groupBox)
-        self.label_4.setObjectName(u"label_4")
-        self.label_4.setGeometry(QRect(10, 100, 81, 16))
+        self.targetDateLabel = QLabel(self.groupBox)
+        self.targetDateLabel.setObjectName(u"targetDateLabel")
+        self.targetDateLabel.setGeometry(QRect(10, 120, 81, 16))
         self.descriptionInput = QTextEdit(self.groupBox)
         self.descriptionInput.setObjectName(u"descriptionInput")
         self.descriptionInput.setGeometry(QRect(220, 20, 191, 71))
@@ -54,8 +54,9 @@ class Ui_GoalsWidget(object):
         self.label_5.setGeometry(QRect(230, 0, 81, 16))
         self.targetDateInput = QDateEdit(self.groupBox)
         self.targetDateInput.setObjectName(u"targetDateInput")
-        self.targetDateInput.setGeometry(QRect(90, 100, 110, 22))
+        self.targetDateInput.setGeometry(QRect(100, 120, 110, 22))
         self.targetDateInput.setButtonSymbols(QAbstractSpinBox.ButtonSymbols.NoButtons)
+        self.targetDateInput.setCalendarPopup(True)
         self.targetAmountInput = QDoubleSpinBox(self.groupBox)
         self.targetAmountInput.setObjectName(u"targetAmountInput")
         self.targetAmountInput.setGeometry(QRect(110, 70, 91, 22))
@@ -63,9 +64,12 @@ class Ui_GoalsWidget(object):
         self.nameInput = QLineEdit(self.groupBox)
         self.nameInput.setObjectName(u"nameInput")
         self.nameInput.setGeometry(QRect(70, 10, 131, 21))
+        self.setTargetDateInput = QCheckBox(self.groupBox)
+        self.setTargetDateInput.setObjectName(u"setTargetDateInput")
+        self.setTargetDateInput.setGeometry(QRect(10, 100, 121, 20))
         self.listWidget = QListWidget(GoalsWidget)
         self.listWidget.setObjectName(u"listWidget")
-        self.listWidget.setGeometry(QRect(30, 150, 411, 181))
+        self.listWidget.setGeometry(QRect(30, 170, 411, 161))
         self.editButton = QPushButton(GoalsWidget)
         self.editButton.setObjectName(u"editButton")
         self.editButton.setGeometry(QRect(270, 330, 81, 32))
@@ -85,8 +89,9 @@ class Ui_GoalsWidget(object):
         self.label.setText(QCoreApplication.translate("GoalsWidget", u"Name", None))
         self.label_2.setText(QCoreApplication.translate("GoalsWidget", u"Target amount", None))
         self.label_3.setText(QCoreApplication.translate("GoalsWidget", u"Pocket", None))
-        self.label_4.setText(QCoreApplication.translate("GoalsWidget", u"Target date", None))
+        self.targetDateLabel.setText(QCoreApplication.translate("GoalsWidget", u"Target date", None))
         self.label_5.setText(QCoreApplication.translate("GoalsWidget", u"Description", None))
+        self.setTargetDateInput.setText(QCoreApplication.translate("GoalsWidget", u"Set target date", None))
         self.editButton.setText(QCoreApplication.translate("GoalsWidget", u"Edit", None))
         self.deleteButton.setText(QCoreApplication.translate("GoalsWidget", u"Delete", None))
     # retranslateUi
--- src/fcontrol/ui/views/goals_widget.py
+++ src/fcontrol/ui/views/goals_widget.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QWidget
+from PySide6.QtCore import Qt, QDate
 
 from fcontrol.ui.views.base import BaseWidget
 from fcontrol.ui.qt_generated.goals_widget import Ui_GoalsWidget
@@ -8,3 +8,26 @@ class GoalsWidget(Ui_GoalsWidget, BaseWidget):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+
+        self._setup_inputs()
+        self._connect_signals()
+
+    def _set_target_date_enabled(self, enabled):
+        self.targetDateInput.setDisabled(not enabled)
+        self.targetDateLabel.setDisabled(not enabled)
+
+    def _setup_inputs(self):
+        self.targetAmountInput.setMinimum(0)
+        self.targetAmountInput.setDecimals(2)
+        self.targetAmountInput.setValue(0.00)
+
+        self.targetDateInput.setMinimumDate(QDate.currentDate())
+        self.targetDateInput.setDate(QDate.currentDate())
+        self._set_target_date_enabled(False)
+
+    def _on_set_target_date_changed(self, state):
+        enabled = Qt.CheckState(state) == Qt.CheckState.Checked
+        self._set_target_date_enabled(enabled)
+
+    def _connect_signals(self):
+        self.setTargetDateInput.stateChanged.connect(self._on_set_target_date_changed)
